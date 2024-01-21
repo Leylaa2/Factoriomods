9a58acd1dd4d861116314a518a6a70eac73cc5a4
Marc <42302732+Leylaa2@users.noreply.github.com>
2024-01-21 15:16:39 +0100

    Add files via upload
    added Small tree collisions and more light in the dark.

---
+++ Leylaa-Mod_0.0.2/data-final-fixes.lua
@@ -0,0 +1,36 @@
+--Science Packs
+--{"automation-science-pack", 1}    --red
+--{"logistic-science-pack", 1}      --green
+--{"chemical-science-pack", 1}      --blue
+--{"military-science-pack", 1}      --grey
+--{"production-science-pack", 1}    --purple
+--{"utility-science-pack", 1}       --yellow
+--{"space-science-pack", 1}         --white
+
+--same research requirements for logistic-system as robots
+data.raw.technology["logistic-system"].unit.ingredients = { 
+    {"automation-science-pack", 1},
+    {"logistic-science-pack", 1},
+    {"chemical-science-pack", 1}
+    --{"utility-science-pack", 1}
+  }
+  
+--prerequisite are now only robot researches, removed production science
+data.raw.technology["logistic-system"].prerequisites = { "logistic-robotics", "construction-robotics" }
+
+--kovarex without purple science
+data.raw.technology["kovarex-enrichment-process"].unit.ingredients = { 
+    {"automation-science-pack", 1},
+    {"logistic-science-pack", 1},
+    {"chemical-science-pack", 1},
+    --{"production-science-pack", 1}
+  }
+  
+--prerequisite are now only rocket fuel and uranium processing, removed production science
+data.raw.technology["kovarex-enrichment-process"].prerequisites = { "rocket-fuel", "uranium-processing" }
+
+-----------------------------------------------------
+--From: https://mods.factorio.com/mod/tree_collision
+for _,tree in pairs(data.raw["tree"]) do
+  tree.collision_box = {{-0.05, -0.05}, {0.05, 0.05}}
+end

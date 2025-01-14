7bc6ef5bfdcd417c5cb667b635a69d47738aa6c6
Marc <42302732+Leylaa2@users.noreply.github.com>
2025-01-14 13:58:31 +0100

    Add files via upload

---
+++ Leylaa-Mod_0.0.4/data.lua
@@ -0,0 +1,9 @@
+data.raw["character"]["character"].light =
+    {
+      {
+        minimum_darkness = 0.3, --Default 0.0
+        intensity = 0.8, --Brightness of the light in the range [0, 1], where 0 is no light and 1 is the maximum light.
+        size = 150, --60The radius of the light in tiles. Note that the light gets darker near the edges, so the effective size of the light will appear to be smaller.
+        --color = {r=1.0, g=1.0, b=1.0}
+      }
+    }
